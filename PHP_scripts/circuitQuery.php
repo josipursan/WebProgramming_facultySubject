52436d351fb43866365d77f7433495375dd2d056
unknown <josip.ursan@gmail.com>
2020-06-22 10:19:26 +0200

    First version of circuitQuery and teamQuery done

--- PHP_scripts/circuitQuery.php
+++ PHP_scripts/circuitQuery.php
@@ -4,6 +4,28 @@
 <head>
     <meta charset = "UTF-8">
     <title>Circuit query</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+
+    <style>
+        caption{
+            caption-side : top;
+            font-size : 2em;
+        }
+
+        body{
+            
+        }
+
+        .table{
+            margin-left: 100%;
+        }
+            
+    </style>
 </head>
 
 <body>
@@ -20,8 +42,13 @@
         $apiCallString = $apiCallRootSection . $circuitsStringFilter . $slash . $selectedCircuit . $slash . $resultsStringFilter . $jsonString;
         $response = file_get_contents($apiCallString);
         $response = json_decode($response, true);
-        print_r($response);
+        //print_r($response);
         //  POZIV KADA GODINA NIJE UNESENA - PRIKAZUJU SE SVI REZULTATI NA ODABRANOJ STAZI
+
+        echo "</br>";
+        echo "</br>";
+        
+        //echo $apiCallString;
     }
     elseif(!isset($selectedCircuit) || trim($selectedCircuit) == '')
     {
@@ -35,30 +62,135 @@
 
         $response = file_get_contents($apiCallString);
         $response = json_decode($response, true);
-        print_r($response);
+        //print_r($response);
         //  POZIV KADA SU UNESENI I GODINA I STAZA
-
         echo "</br>";
         echo "</br>";
         
-        echo $apiCallString;
+        //echo $apiCallString;
     }
 
-    echo $apiCallString;
     echo "</br>";
 
-    echo $selectedYear;
-    echo "</br>";
-    
-    echo $selectedCircuit;
-    echo "</br>";
-    
+    $season = isset($response['MRData']['RaceTable']['season']) ? $response['MRData']['RaceTable']['season'] : '/' ;
+    $roundInSeason = isset($response['MRData']['RaceTable']['Races'][0]['round']) ? $response['MRData']['RaceTable']['Races'][0]['round'] : '/';
+    $circuitName = isset($response['MRData']['RaceTable']['Races'][0]['Circuit']['circuitName']) ? $response['MRData']['RaceTable']['Races'][0]['Circuit']['circuitName'] : '/';
+    $country = isset($response['MRData']['RaceTable']['Races'][0]['Circuit']['Location']['country']) ? $response['MRData']['RaceTable']['Races'][0]['Circuit']['Location']['country'] : '/';
+    $raceDate = isset($response['MRData']['RaceTable']['Races'][0]['date']) ? $response['MRData']['RaceTable']['Races'][0]['date'] : '/';
+
+    $basicDataArray = array($season, $roundInSeason, $circuitName, $country, $raceDate);
+    $basicData_tableHeader = array('Season', 'Round', 'Circuit name', 'Country', 'Date');
 
     
-    
+    $firstLayer = $response['MRData']['RaceTable']['Races'][0]['Results'];
+
+    $data = array(
+        array(),    //stavi tu imenovanje, i promijeni sto je potrebno u funkciji koja builda tablicu ("firstNames => array(), ....)
+        array(),
+        array(),
+        array(),
+        array(),
+        array()
+    );    //Polje u koje se trpaju imena, prezimena, ekipe, najbolja vremena, itd... za prikaz u tablici. 
+
+    $numberOfDrivers = 1;   // Ovaj blok provjerava koliko ima vozaca
+    while(true)     
+    {
+        if(!array_key_exists($numberOfDrivers, $firstLayer))
+        {
+            break;
+        }
+        else
+        {
+            $numberOfDrivers += 1;
+            continue;
+        }
+    }
+
+    for($i = 0; $i < $numberOfDrivers; $i++)
+    {
+        isset($firstLayer[$i]["Driver"]["givenName"]) ? array_push($data[0], $firstLayer[$i]["Driver"]["givenName"]) : array_push($data[0], '/');
+        isset($firstLayer[$i]["Driver"]["familyName"]) ? array_push($data[1], $firstLayer[$i]["Driver"]["familyName"]) : array_push($data[1], '/');   
+        isset($firstLayer[$i]["Constructor"]["name"]) ? array_push($data[2], $firstLayer[$i]["Constructor"]["name"]) : array_push($data[2], '/');
+        isset($firstLayer[$i]["FastestLap"]["Time"]["time"]) ? array_push($data[3], $firstLayer[$i]["FastestLap"]["Time"]["time"]) : array_push($data[3], '/');
+        isset($firstLayer[$i]["FastestLap"]["AverageSpeed"]["speed"]) ? array_push($data[4], $firstLayer[$i]["FastestLap"]["AverageSpeed"]["speed"]) : array_push($data[4], '/');
+        isset($firstLayer[$i]["status"]) ? array_push($data[5], $firstLayer[$i]["status"]) : array_push($data[$i], '/');
+
+        // PRIMJER PUSHANJA U data ARRAY BEZ "ERROR HANDLINGA" ZA SLUCAJEVE KADA ODREDENI VOZACI NEMAJU ODREDENE PARAMETRE DEFINIRANE POPUT AVG.SPEED ITD... : 
+        //                                          array_push($data[5], $firstLayer[$i]["status"]);
+    }
 
 
+    buildBasicInfoTable($basicDataArray, $basicData_tableHeader);
 
+    buildCircuitResultTable($data);
+
+
+    /////////////////////////////////////////\\///   ˇˇ FUNCTIONS ˇˇ  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+    function buildBasicInfoTable($basicDataArray, $basicData_tableHeader)
+    {
+        echo "<div class = \"container-fluid text-center\">";
+            echo "<div class = \"row\">";
+                echo "<div class = \"col-sm-4\">";
+
+                    echo "<table class= \"table table-striped\">";
+                    echo "<caption>Basic race information</caption>";
+                    for($i = 0; $i < count($basicDataArray); $i++)
+                    {
+                        echo "<tr>";
+                        echo "<th>$basicData_tableHeader[$i]</th>";
+                        echo "<td>$basicDataArray[$i]</td>";
+                        echo "</tr>";
+                    }
+                    echo "</table>";
+
+                echo "</div>";
+            echo "</div>";
+        echo "</div>";
+    }
+
+    function buildCircuitResultTable($twoDimArray_data)
+    {
+        $tableHeadersArray = array("Name", "Last name", "Team", "Fastest Lap", "Avg. speed (kph)", "Status");
+
+        echo "<div class = \"container-fluid text-center\">";
+            echo "<div class = \"row\">";
+                echo "<div class = \"col-sm-4\">";
+
+                    echo "<table class= \"table table-striped\">";
+                    echo "<caption>Race results</caption>";
+                    echo "<tr>";
+                    for($i = 0; $i < count($tableHeadersArray); $i++)
+                    {
+                        echo "<th>$tableHeadersArray[$i]</th>";
+                    }
+                    echo "</tr>";
+                    
+                    for($i = 0; $i < count($twoDimArray_data[0]); $i++)
+                    {
+                            $name = $twoDimArray_data[0][$i];
+                            $lastName = $twoDimArray_data[1][$i];
+                            $team = $twoDimArray_data[2][$i];
+                            $fastestLap = $twoDimArray_data[3][$i];
+                            $averageSpeed = $twoDimArray_data[4][$i];
+                            $status = $twoDimArray_data[5][$i];
+
+                            echo "<tr>";
+                            echo "<td>$name</td>"; 
+                            echo "<td>$lastName</td>";
+                            echo "<td>$team</td>";
+                            echo "<td>$fastestLap</td>";
+                            echo "<td>$averageSpeed</td>";
+                            echo "<td>$status</td>";                       
+                            echo "</tr>";
+                    }
+                    echo "</table>";
+
+                    echo "</div>";
+            echo "</div>";
+        echo "</div>";
+    }
 
 
 
@@ -83,23 +215,7 @@
             -2. poziv nije bas mudar, mnoge utrke poput Monze, Spa, Silverstone-a, itd. se odrzavaju desetljecima, a bas da se sve to parsa, i prikazuje u prikladnim tablicama nije pametno
 
 
-
-    $apiCallRootSection = "http://ergast.com/api/f1/";
-    $slash = "/";
-
-    $driverStringFilter = "drivers";
-    $construstorStringFilter = "constructors";
-    $resultsStringFilter = "results";
-    $circuitsStringFilter = "circuits";
-
-
-
-    $jsonString = ".json";
-
     */
-
-
-
     ?>
 
 </body>
--- PHP_scripts/teamQuery.php
+++ PHP_scripts/teamQuery.php
@@ -4,6 +4,23 @@
 <head>
     <meta charset = "UTF-8">
     <title>Circuit query</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+    <style>
+        caption{
+            caption-side : top;
+            font-size : 2em;
+        }
+
+        .table{
+            margin-left: 100%;
+        }
+            
+    </style>
 </head>
 
 <body>
@@ -44,14 +61,106 @@
         $apiCallString = $apiCallRootSection . $yearInput . $slash . $constructorStringFilter . $slash . $teamInput . $slash . $circuitsStringFilter . $slash . $cicruitInput . $slash . $resultsStringFilter . $jsonString;
         $response = file_get_contents($apiCallString);
         $response = json_decode($response, true);
-        print_r($response);
+        //print_r($response);
         // BUILD CIJELOG POZIVA SA SVIM POLJIMA UNESENIM
     }
 
-    echo "</br>";
-    echo "</br>";
-    echo "</br>";
     echo $apiCallString;
+
+    $firstLayer = $response['MRData']['RaceTable']['Races'][0]['Results'];
+
+    $data = array(
+        array(),    //first name    0
+        array(),    // last name    1
+        array(),    //constructor   2
+        array(),    //laps          3
+        array(),    //starting pos. 4
+        array(),    //time          5
+        array(),    //status        6
+        array(), //points           7
+        array() //finishing pos.    8
+    );
+
+    $numberOfDrivers = 1;   // Ovaj blok provjerava koliko ima vozaca
+    while(true)     
+    {
+        if(!array_key_exists($numberOfDrivers, $firstLayer))
+        {
+            break;
+        }
+        else
+        {
+            $numberOfDrivers += 1;
+            continue;
+        }
+    }
+
+    for($i = 0; $i < $numberOfDrivers; $i++)
+    {
+        isset($firstLayer[$i]["Driver"]["givenName"]) ? array_push($data[0], $firstLayer[$i]["Driver"]["givenName"]) : array_push($data[0], '/');
+        isset($firstLayer[$i]["Driver"]["familyName"]) ? array_push($data[1], $firstLayer[$i]["Driver"]["familyName"]) : array_push($data[1], '/'); 
+        isset($firstLayer[$i]["Constructor"]["name"]) ? array_push($data[2], $firstLayer[$i]["Constructor"]["name"]) : array_push($data[2], '/');
+        isset($firstLayer[$i]['laps']) ? array_push($data[3], $firstLayer[$i]['laps']) : '/';
+        isset($firstLayer[$i]['grid']) ? array_push($data[4], $firstLayer[$i]['grid']) : '/';
+        isset($firstLayer[$i]['status']) ? array_push($data[5], $firstLayer[$i]['status']) : '/';
+        isset($firstLayer[$i]['points']) ? array_push($data[6], $firstLayer[$i]['points']) : '/';
+        isset($firstLayer[$i]['positionText']) ? array_push($data[7], $firstLayer[$i]['positionText']) : '/';
+        // PRIMJER PUSHANJA U data ARRAY BEZ "ERROR HANDLINGA" ZA SLUCAJEVE KADA ODREDENI VOZACI NEMAJU ODREDENE PARAMETRE DEFINIRANE POPUT AVG.SPEED ITD... : 
+        //                                          array_push($data[5], $firstLayer[$i]["status"]);
+    }
+
+    buildTable($data);
+
+
+    /////////////////////////////////////////////   ˇˇ FUNCTIONS ˇˇ  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+    function buildTable($twoDimDataArray)
+    {
+        $tableHeadersArray = array('First name', 'Last name', 'Constructor', 'Laps', 'Starting position', 'Status', 'Points', 'Finishing position');
+
+        echo "<div class = \"container-fluid text-center\">";
+            echo "<div class = \"row\">";
+                echo "<div class = \"col-sm-4\">";
+
+                echo "<table class= \"table table-striped\">";
+                    echo "<caption>Race results</caption>";
+                    echo "<tr>";
+                    for($i = 0; $i < count($tableHeadersArray); $i++)
+                    {
+                        echo "<th>$tableHeadersArray[$i]</th>";
+                    }
+                    echo "</tr>";
+
+                    for($i = 0; $i < count($twoDimDataArray[0]); $i++)
+                    {
+                            $name = $twoDimDataArray[0][$i];
+                            $lastName = $twoDimDataArray[1][$i];
+                            $team = $twoDimDataArray[2][$i];
+                            $laps = $twoDimDataArray[3][$i];
+                            $startingPosition = $twoDimDataArray[4][$i];
+                            $status = $twoDimDataArray[5][$i];
+                            $points = $twoDimDataArray[6][$i];
+                            $finishingPosition = $twoDimDataArray[7][$i];
+
+                            echo "<tr>";
+                            echo "<td>$name</td>"; 
+                            echo "<td>$lastName</td>";
+                            echo "<td>$team</td>";
+                            echo "<td>$laps</td>";
+                            echo "<td>$startingPosition</td>";
+                            echo "<td>$status</td>";
+                            echo "<td>$points</td>";
+                            echo "<td>$finishingPosition</td>";
+                            echo "</tr>";
+                    }
+
+                echo "</table>";
+
+                echo "</div>";
+            echo "</div>";
+        echo "</div>";
+
+    }
 
 
 
