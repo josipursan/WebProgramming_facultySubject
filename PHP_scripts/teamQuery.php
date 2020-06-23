d46ee7bb44e68ab2a72744cb94cbd3fc5ff9c346
unknown <josip.ursan@gmail.com>
2020-06-23 17:32:22 +0200

    Team query PHP script done - driver query remains

--- PHP_scripts/teamQuery.php
+++ PHP_scripts/teamQuery.php
@@ -31,18 +31,56 @@
     $teamInput = filter_input(INPUT_GET, "teamInput");  // za sada je tu maknut FILTER_SANITIZE_STRING jer on uklanja _ ako je prisutan, a ponekad je potreban za id-eve Ergast API-ja
     $cicruitInput = filter_input(INPUT_GET, "circuitInput");    // za sada je tu maknut FILTER_SANITIZE_STRING jer on uklanja _ ako je prisutan, a ponekad je potreban za id-eve Ergast API-ja
 
-    if(!isset($yearInput) || trim($yearInput) == '')
+    if(!isset($cicruitInput) || trim($cicruitInput) == '')
     {
-        $apiCallString = $apiCallRootSection . $constructorStringFilter . $slash . $teamInput . $slash . $circuitsStringFilter . $slash . $cicruitInput . $slash . $resultsStringFilter . $jsonString ;
+        $apiCallString_additionToGetAllRacesInOneGo = '?limit=50';
+
+        $apiCallString = $apiCallRootSection . $yearInput . $slash . $constructorStringFilter . $slash . $teamInput . $slash . $resultsStringFilter . $jsonString . $apiCallString_additionToGetAllRacesInOneGo;
         $response = file_get_contents($apiCallString);
         $response = json_decode($response, true);
-        // BUILD POZIVA KAD GODINA NIJE UNESENA
-    }
-    elseif(!isset($cicruitInput) || trim($cicruitInput) == '')
-    {
-        $apiCallString = $apiCallRootSection . $yearInput . $slash . $constructorStringFilter . $slash . $teamInput . $slash . $resultsStringFilter . $jsonString;
-        $response = file_get_contents($apiCallString);
-        $response = json_decode($response, true);
+        
+        $firstLayer = $response['MRData']['RaceTable']['Races'];
+
+        $data = array(
+            array(),    //race list
+            array(),    //firstName
+            array(),    //lastName
+            array(),    //laps
+            array(),    //status
+            array(),    //points,
+            array()     //position
+        );
+
+        $numberOfRaces = 1;
+        while(true)     
+        {
+            if(!array_key_exists($numberOfRaces, $firstLayer))
+            {
+                break;
+            }
+            else
+            {
+                $numberOfRaces += 1;
+            }
+        }
+
+        for($i = 0; $i < $numberOfRaces; $i++)
+        {
+            isset($firstLayer[$i]['raceName']) ? array_push($data[0], $firstLayer[$i]['raceName']) : '/';
+        }
+        for($i = 0; $i < $numberOfRaces; $i++)
+        {
+            for($j = 0; $j < 2; $j++)
+            {
+                isset($firstLayer[$i]['Results'][$j]['Driver']['givenName']) ? array_push($data[1], $firstLayer[$i]['Results'][$j]['Driver']['givenName']) : '/';
+                isset($firstLayer[$i]['Results'][$j]['Driver']['familyName']) ? array_push($data[2], $firstLayer[$i]['Results'][$j]['Driver']['familyName']) : '/';
+                isset($firstLayer[$i]['Results'][$j]['laps']) ? array_push($data[3], $firstLayer[$i]['Results'][$j]['laps']) : '/';
+                isset($firstLayer[$i]['Results'][$j]['status']) ? array_push($data[4], $firstLayer[$i]['Results'][$j]['status']) : '/';
+                isset($firstLayer[$i]['Results'][$j]['points']) ? array_push($data[5], $firstLayer[$i]['Results'][$j]['points']) : '/';
+                isset($firstLayer[$i]['Results'][$j]['positionText']) ? array_push($data[6], $firstLayer[$i]['Results'][$j]['positionText']) : '/';
+            }
+        }
+        buildTable_yearAndTeam($data);
         // BUILD POZIVA KAD STAZA NIJE UNESENA
     }
     else
@@ -83,6 +121,8 @@
 
         // BUILD CIJELOG POZIVA SA SVIM POLJIMA UNESENIM
     }
+
+    echo $apiCallString;
 
 
     
@@ -137,9 +177,62 @@
         echo "</div>";
     }
 
-    function buildTable_onlyYear($twoDimDataArray)
+    function buildTable_yearAndTeam($twoDimDataArray)
     {
-        // TO DO...
+        $tableHeadersArray = array('Race list', 'First name', 'Last name', 'Laps', 'Status', 'Points', 'Position');
+
+        echo "<div class = \"container-fluid text-center\">";
+            echo "<div class = \"row\">";
+                echo "<div class = \"col-sm-4\">";
+
+                
+                for($i = 0; $i < count($twoDimDataArray[0]); $i++)
+                {
+                    echo "<table class= \"table table-striped\">";
+                    $tableCaption_raceName = $twoDimDataArray[0][$i];
+                    echo "<tr>";
+                    echo "<caption>$tableCaption_raceName</caption>";
+                    echo "</tr>";
+
+                    echo "<tr>";
+                    for($z = 1; $z < count($tableHeadersArray); $z++)
+                    {
+                        echo "<th>$tableHeadersArray[$z]</th>";
+                    }
+                    echo "</tr>";
+
+                    
+                    for($j = 0; $j < 2; $j++)
+                    {
+                        $firstName = $twoDimDataArray[1][$j];
+                        $lastName = $twoDimDataArray[2][$j];
+                        $laps = $twoDimDataArray[3][$j];
+                        $status = $twoDimDataArray[4][$j];
+                        $points = $twoDimDataArray[5][$j];
+                        $position = $twoDimDataArray[6][$j];
+
+                        echo "<tr>";
+                        echo "<td>$firstName</td>";
+                        echo "<td>$lastName</td>";
+                        echo "<td>$laps</td>";
+                        echo "<td>$status</td>";
+                        echo "<td>$points</td>";
+                        echo "<td>$position</td>";
+                        echo "</tr>";
+                        
+                        
+                    }
+
+                    for($k = 1; $k < 7; $k++)
+                    {
+                        $twoDimDataArray[$k] = array_slice($twoDimDataArray[$k], 2);
+                    }
+                    echo "</table>";
+                }
+            
+                echo "</div>";
+            echo "</div>";
+        echo "</div>";
     }
 
 
