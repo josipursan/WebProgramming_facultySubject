1aaa32507d3fcf21278f41d274c6517db2c7d808
unknown <josip.ursan@gmail.com>
2020-06-24 14:47:36 +0200

    First full version of driver, team and circuit queries.

--- PHP_scripts/driverQuery.php
+++ PHP_scripts/driverQuery.php
@@ -1,63 +1,156 @@
-<?php
+<!DOCTPYE html>
+<html lang = "en-US">
 
-/*
-    Pokupiti korisnicki unos za prezime vozaca, sanitazati taj unos da nisu utrpane neke gluposti ili SQL injeciton. 
-    Low casati unos i napraviti sljedeci poziv na Ergast API : http://ergast.com/api/f1/drivers/webber
+<head>
+    <meta charset = "UTF-8">
+    <title>Circuit query</title>
 
-    Ako ovaj poziv vrati vrati neku vrijednost, i ne radi se o BAD REQUEST vrijednosti, onda idemo dalje na glavni poziv ove skripte. 
-    Ovaj gornji poziv je zapravo samo provjera da korisnik nije unio neki glupi string ili promašio ime tipa "VeTel" umjesto "Vettel".
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 
-    Ista ova logika primjenit ce se za korisnicki unos staze i ekipe.
-    Sanitazati korisnicki unos imena staze, low casati i provjeriti na Ergast API hoce li to ime staze vratiti nesto, ili ce baciti BAD REQUEST.
+    <style>
+        caption{
+            caption-side : top;
+            font-size : 2em;
+        }
+
+        body{
+            
+        }
+
+        .table{
+            margin-left: 100%;
+        }    
+    </style>
+</head>
+
+<body>
+
+
+    <?php
+    require "APICALL_KEYWORDS.php";
+
+    $year = filter_input(INPUT_GET, "yearInput");
+    $lastName = filter_input(INPUT_GET, "lastName");
+
+    if((isset($year) || !empty($year)) && (isset($lastName) || !empty($lastName)))
+    {
+        $apiCallString_additionToGetAllRacesInOneGo = '?limit=50';
+
+        $apiCallString = $apiCallRootSection . $year . $slash . $driverStringFilter . $slash . $lastName . $slash . $resultsStringFilter . $jsonString . $apiCallString_additionToGetAllRacesInOneGo;
+        $response = file_get_contents($apiCallString);
+        $response = json_decode($response, true);
+        
+        
+        $response = file_get_contents($apiCallString);
+        $response = json_decode($response, true);
+        
+        $firstLayer = $response['MRData']['RaceTable']['Races'];
+
+        $data = array(
+            array(),    //race list
+            array(),    //firstName
+            array(),    //lastName
+            array(),    //laps
+            array(),    //status
+            array(),    //points,
+            array()     //position
+        );
+
+        $numberOfRaces = 1;
+        while(true)     
+        {
+            if(!array_key_exists($numberOfRaces, $firstLayer))
+            {
+                break;
+            }
+            else
+            {
+                $numberOfRaces += 1;
+            }
+        }
+
+        for($i = 0; $i < $numberOfRaces; $i++)
+        {
+            isset($firstLayer[$i]['raceName']) ? array_push($data[0], $firstLayer[$i]['raceName']) : '/';
+        }
+        for($i = 0; $i < $numberOfRaces; $i++)
+        {
+            for($j = 0; $j < 1; $j++)
+            {
+                isset($firstLayer[$i]['Results'][$j]['Driver']['givenName']) ? array_push($data[1], $firstLayer[$i]['Results'][$j]['Driver']['givenName']) : '/';
+                isset($firstLayer[$i]['Results'][$j]['Driver']['familyName']) ? array_push($data[2], $firstLayer[$i]['Results'][$j]['Driver']['familyName']) : '/';
+                isset($firstLayer[$i]['Results'][$j]['laps']) ? array_push($data[3], $firstLayer[$i]['Results'][$j]['laps']) : '/';
+                isset($firstLayer[$i]['Results'][$j]['status']) ? array_push($data[4], $firstLayer[$i]['Results'][$j]['status']) : '/';
+                isset($firstLayer[$i]['Results'][$j]['points']) ? array_push($data[5], $firstLayer[$i]['Results'][$j]['points']) : '/';
+                isset($firstLayer[$i]['Results'][$j]['positionText']) ? array_push($data[6], $firstLayer[$i]['Results'][$j]['positionText']) : '/';
+            }
+        }
+        buildTable_driverResults($data);
+        echo $apiCallString;
+    }
+
+
+    function buildTable_driverResults($twoDimDataArray)
+    {
+        $tableHeadersArray = array('Race list', 'First name', 'Last name', 'Laps', 'Status', 'Points', 'Position');
+
+        echo "<div class = \"container-fluid text-center\">";
+            echo "<div class = \"row\">";
+                echo "<div class = \"col-sm-4\">";
+
+                
+                for($i = 0; $i < count($twoDimDataArray[0]); $i++)
+                {
+                    echo "<table class= \"table table-striped\">";
+                    $tableCaption_raceName = $twoDimDataArray[0][$i];
+                    echo "<tr>";
+                    echo "<caption>$tableCaption_raceName</caption>";
+                    echo "</tr>";
+
+                    echo "<tr>";
+                    for($z = 1; $z < count($tableHeadersArray); $z++)
+                    {
+                        echo "<th>$tableHeadersArray[$z]</th>";
+                    }
+                    echo "</tr>";
+
+                    
+                    for($j = 0; $j < 1; $j++)
+                    {
+                        $firstName = $twoDimDataArray[1][$j];
+                        $lastName = $twoDimDataArray[2][$j];
+                        $laps = $twoDimDataArray[3][$j];
+                        $status = $twoDimDataArray[4][$j];
+                        $points = $twoDimDataArray[5][$j];
+                        $position = $twoDimDataArray[6][$j];
+
+                        echo "<tr>";
+                        echo "<td>$firstName</td>";
+                        echo "<td>$lastName</td>";
+                        echo "<td>$laps</td>";
+                        echo "<td>$status</td>";
+                        echo "<td>$points</td>";
+                        echo "<td>$position</td>";
+                        echo "</tr>";
+                        
+                        
+                    }
+
+                    for($k = 1; $k < 7; $k++)
+                    {
+                        $twoDimDataArray[$k] = array_slice($twoDimDataArray[$k], 1);
+                    }
+                    echo "</table>";
+                }
+            
+                echo "</div>";
+            echo "</div>";
+        echo "</div>";
+    }
     
+    ?>
 
-*/
-
-
-
-/*  PRIMJERI MOGUCIH QUERYJA : 
-
-    Korisnik moze unijeti slijedece podatke : godina, prezime vozaca, staza, ekipa
-    
-
-
-    1. Korisnik je odabrao sva polja za query. Moguca polja su godina, prezime vozaca, ekipa, staza.
-
-        https://ergast.com/api/f1/2012/drivers/alonso/constructors/ferrari/circuits/monza/results   -----> ovime se prikazuju rezultati odabranog vozaca za odabranu sezonu na odabranoj stazi za odabranu ekipu (isto kao 2. poziv ali duze)
-
-    2. Korisnik je odabrao polja godina, prezime vozaca, staza. 
-
-        https://ergast.com/api/f1/2012/drivers/alonso/circuits/monza/results    ------> ovime se prikazuju rezultati odabranog vozaca za odabranu sezonu na odabranoj stazi (isto kao 1. poziv ali krace jer je uvjet ekipe redundantan)
-
-    3. Korisnik je odabrao polja prezime vozaca, staza. 
-
-        https://ergast.com/api/f1/drivers/alonso/circuits/monza/results     --------> ovime se prikazuju svi rezultati vozaca u karijeri za odabranu stazu
-
-    4. Korisnik je odabrao samo prezime vozaca. 
-
-        https://ergast.com/api/f1/drivers/alonso/results    -------> ovime se prikazuju svi rezultati vozaca (cijela karijera)
-
-    5. Korisnik je odabrao godinu, vozaca i ekipu.
-    
-        https://ergast.com/api/f1/2012/drivers/alonso/constructors/ferrari/results  ------> ovime se prikazuju svi rezultati odabranog vozaca u odabranoj sezoni za odabranu ekipu
-
-    6. Korinik je odabrao godinu i vozaca. 
-
-        https://ergast.com/api/f1/2012/drivers/alonso/results   ------> ovime se prikazuju svi rezultati odabranog vozaca u odabranoj sezoni
-
-    7. Korisnik je odabrao vozaca i ekipu.
-
-        https://ergast.com/api/f1/drivers/alonso/constructors/ferrari/results    ------> ovime se prikazuju svi rezultati za odabranog vozaca s odabranom ekipom
-
-    Komentari : 
-        -pozivi 1. i 2. daju isti rezultat jer je parametar ekipe u 1. pozivu zapravo redundantan obzirom da se zna za koju je ekipu vozio ako je definirana sezona
-        -pozivi 5. i 6. daju isti rezultat jer je parametar ekipe u 5. pozivu zapravo redundantan obzirom da se zna za koju je ekipu vozio ako je definirana sezona
-
-        -poziv 4. mozda treba onemoguciti obzirom da bi se radilo o velikom broju rezultata za pojedine vozace za prikaz
-
-
-        ** AKO SE ZELI POKUPITI REZULTATE U OBLIKU JSON-A, NA KRAJ TREBA DODATI ".json"
-*/
-
-
-?>
+</body>
